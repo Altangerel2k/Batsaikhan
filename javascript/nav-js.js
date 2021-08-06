56a775c8ae25537a99194506cc2c0cacba13507b
Batsaikhan <batsaikhan0408@gmail.com>
2021-08-06 16:30:10 +0800

    web huudas

---
+++ javascript/nav-js.js
@@ -0,0 +1,10 @@
+function clickMobileMenu()
+{
+    let x = document.getElementById("bs-navigation");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
